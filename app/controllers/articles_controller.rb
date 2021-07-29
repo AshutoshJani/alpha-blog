7767fba79c0cfdfd3f9512512c0154418d1c9838
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-07-29 17:44:53 +0530

    Added functionality of creating new article and editing existing article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,11 +9,36 @@ class ArticlesController < ApplicationController
     end
 
     def new
-
+        @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+
     def create
-        
+        @article = Article.new(params.require(:article).permit(:title, :description)) #cannot provide parameters directly due to security so need to specify the columns (this is called strong parameters and whitelisting data)
+        # render plain: @article.inspect #prints out the value of @article on the browser
+        if @article.save
+            #flash is used to display 'alert' or 'notice' messages in the web page
+            flash[:notice] = "Article was created successfully." #flash is a hash type data structure and is usually enables in view/layouts/application.html.erb since that is shared between all the html pages
+
+            # redirect_to article_path(@article) #article_path is the path received from the url 
+            redirect_to @article #shorter method to redirect to the newly created article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+            flash[:notice] = "Article was updated successfully"
+            redirect_to @article
+        else
+            render 'edit'
+        end
     end
 
 end
