bb64a17498f8af9b95bf75c1321f5e8be43df3ae
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-09 16:17:09 +0530

    Updated UI and added Users show page and links related to it

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+# Font-awesome
+gem "font-awesome-rails"
+
 group :development, :test do
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3', '~> 1.4'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,15 @@ class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
 
     def new
-
+        @user = User.new
     end
 
     def show
         @articles = @user.articles
+    end
+
+    def index
+        @users = User.all
     end
 
     def create
