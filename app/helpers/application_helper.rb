713cef1d4c3362f9ec9ac65a839456daec76f0c4
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-10 11:35:41 +0530

    Added user session to keep track of logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
         image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
     end
 
+    def current_user
+        # This function sets the value of current user once and returns the value of the current logged in user everytime its called, this reduces the calls made to the database to find current user 
+        @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    end
+
+    def logged_in?
+        # !! is used to create object into boolean, so if object has a value ir returns true else false
+        !!current_user
+    end
+    
 end
