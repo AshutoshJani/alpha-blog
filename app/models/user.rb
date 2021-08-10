1e77300753e3de0dd8b4934ffccd05cfddbbd9b7
AshutoshJani <ashutosh.jani.42@gmail.com>
2021-08-10 16:46:42 +0530

    Added user delete functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     before_save { self.email = email.downcase }
 
     # Many to one association
-    has_many :articles
+    has_many :articles, dependent: :destroy
+    # dependant: :destroy means if a user is deleted all the articles associated with the user will be deleted as well
 
     validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
 
